5fe4cb9923217f54e29872611f352a4c0c7e9b11
{miakim} <miakimsungji@gmail.com>
2023-03-14 11:21:47 +0900

    ver 08

--- www/mobile/usethis.php
+++ www/mobile/usethis.php
@@ -7,6 +7,7 @@
   <p>모바일 버전입니다.여기에 내용 넣어주세요</p>
   <p>폴더명 PLZ -> shinhan 으로 수정해 주세요 </p>
   <p>경로 틀어지면 성지를 찾으세호잇호잇</p>
+  <p>호오오오오시</p>
 
   <?php 
   print_r ($_SERVER['DOCUMENT_ROOT'].'/shinhan/www/mobile/css/m_common.css');
