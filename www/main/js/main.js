44b835105ece722daef7a1770833dc37e1198b03
YE0414 <yna980414@gmail.com>
2023-03-14 10:59:08 +0900

    lottee is awesome;

--- www/main/js/main.js
+++ www/main/js/main.js
@@ -131,14 +131,96 @@ $(document).ready(function() {
   }, function() {
     $(this).find('a').remove();
   });
-
-
-
 });
 
+// 쿠키만들기
+$(function() {
+  if($.cookie('popup')=='none') {
+      // 쿠키 파일이 현재 브라우저에 저장되어 있는지를 확인하여 none이면 모달이 안나오게 한다.
+      $('#modal').hide();
+  }
 
+  
 
+  //오늘하루열지않음을 클릭시쿠키가 생성이 되고 그렇지 않으면 그냥 모달을 숨기는 내용
+function closePop() {
+let ch = $('#ch'); // 체크박스 변수
+if(ch.is(':checked')) {
+  alert('체크박스');
+    $.cookie('popup', 'none', {expires:7, path:'/'}); // 쿠키를 생성한다.
+}
+$('#modal').hide();
+}
 
+$('#modal>#banner>p>label').click(function(){
+$.cookie('popup', 'none', {expires:7, path:'/'}); // 쿠키를 생성한다.
+})
+
+$('#modal a').click(function(){
+$('#modal').fadeOut();
+})
+});
+//슬라이드
+$(function(){
+  //1. 변수선언
+    let slide = $('#banner #img');
+    let l_btn = $('#banner .bi-chevron-left');
+    let r_btn = $('#banner .bi-chevron-right');
+    let img_w = 450;
+    let n  = $('.mc_btn>li').index();
+    // console.log(n);
+
+    function moveLeft(n){
+        slide.stop().animate({left:-img_w*n},300);  //0 원래위치로 돌아옴.
+        $('.mc_btn li').removeClass('on');
+        $('.mc_btn li').eq(n).addClass('on');
+        // console.log('moveleft!')
+    }
+
+    Timer = setInterval(function(){
+      if(n==0){
+        n=1;
+      }else{
+        n=0;
+      };
+      moveLeft(n);}
+      ,5000
+    );
+
+    l_btn.click(function(){
+      clearInterval(Timer);
+    },function(){
+      if(n==0){
+        n=1;
+      }else{
+        n=0;
+      };
+      moveLeft(n);}
+      ,5000);
+
+    r_btn.click(function(){
+      clearInterval(Timer);
+    },function(){
+      if(n==0){
+        n=1;
+      }else{
+        n=0;
+      };
+      moveLeft(n);}
+      ,5000);
+
+    $('.c_btn li:first-child').click(function(){
+      clearInterval(Timer);
+    },function(){
+      moveLeft(0);}
+      ,5000);
+
+  $('.c_btn li:last-child').click(function(){
+      clearInterval(Timer);
+    },function(){
+      moveLeft(1);}
+      ,5000);
+  });
 
   
 
